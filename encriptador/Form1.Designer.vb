84317ff0c1de5d5e4e5f84cc2cfc5b8a900556b6
Alberto <alber1183@hotmail.com>
2016-03-28 18:39:55 +0200

    Ficheros

---
+++ encriptador/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog2 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog3 = New System.Windows.Forms.OpenFileDialog()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "txt"
+        Me.OpenFileDialog1.FileName = "Archivo a encriptar"
+        Me.OpenFileDialog1.Filter = "Texto |*.txt"
+        Me.OpenFileDialog1.Title = "Archivo que se desea encriptiar"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.DefaultExt = "bmp"
+        Me.OpenFileDialog2.FileName = "Base para enmascarar"
+        Me.OpenFileDialog2.Filter = "BMP |*.bmp"
+        Me.OpenFileDialog2.Title = "Archivo que se va a utilizar como enmascaramiento"
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.FileName = "Archivo final"
+        Me.SaveFileDialog1.Title = "Archivo encriptado"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(279, 124)
+        Me.TabControl1.TabIndex = 3
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.TabPage1.BackgroundImage = Global.encriptador.My.Resources.Resources.fondo
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Controls.Add(Me.Button3)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(271, 98)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Encriptar"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(260, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Archivo a encriptar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(7, 69)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(259, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Encriptar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(6, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(260, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Base para enmascarar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackgroundImage = Global.encriptador.My.Resources.Resources.fondo32
+        Me.TabPage2.Controls.Add(Me.Button5)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(271, 98)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Desencriptar"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(6, 35)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(259, 23)
+        Me.Button5.TabIndex = 3
+        Me.Button5.Text = "Archivo desencriptado"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(6, 6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(259, 23)
+        Me.Button4.TabIndex = 2
+        Me.Button4.Text = "Archivo"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog2
+        '
+        Me.SaveFileDialog2.Filter = "Texto|*.txt"
+        Me.SaveFileDialog2.Title = "Archivo desencriptado"
+        '
+        'OpenFileDialog3
+        '
+        Me.OpenFileDialog3.FileName = "Archivo encriptado"
+        Me.OpenFileDialog3.Filter = "BMP |*.bmp"
+        Me.OpenFileDialog3.Title = "Archivo a desencriptar"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BackgroundImage = Global.encriptador.My.Resources.Resources.fondo2
+        Me.ClientSize = New System.Drawing.Size(279, 124)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Encriptador"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog2 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog2 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents OpenFileDialog3 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ encriptador/Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Dim hex_txt As String
+    Dim hex_bmp As String
+    Dim hex_final As String
+    Public Function StringToHex(ByVal StrToHex As String) As String
+        Dim strTemp As String
+        Dim strReturn As String
+        Dim I As Long
+        For I = 1 To Len(StrToHex)
+            strTemp = Hex(Asc(Mid$(StrToHex, I, 1)))
+            If Len(strTemp) = 1 Then strTemp = "0" & strTemp
+            strReturn = strReturn & strTemp
+        Next I
+        StringToHex = strReturn
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            hex_txt = String.Join("", IO.File.ReadAllBytes(OpenFileDialog1.FileName).Select(Function(b) b.ToString("X2")).ToArray()) & "01"
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        If OpenFileDialog2.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            hex_bmp = String.Join("", IO.File.ReadAllBytes(OpenFileDialog2.FileName).Select(Function(b) b.ToString("X2")).ToArray())
+            If hex_txt.Length / 2 <= (((hex_bmp.Length / 2) - 54) / 6) ^ 2 Then
+                Button3.Enabled = True
+            Else
+                MsgBox("Imagen demasiado pequeña")
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        SaveFileDialog1.FileName = System.IO.Path.GetFileNameWithoutExtension(OpenFileDialog2.FileName)
+        SaveFileDialog1.DefaultExt = System.IO.Path.GetExtension(OpenFileDialog2.FileName)
+        SaveFileDialog1.Filter = "Archivo " & System.IO.Path.GetExtension(OpenFileDialog2.FileName).Replace(".", "") & "|*." & System.IO.Path.GetExtension(OpenFileDialog2.FileName).Replace(".", "")
+        If SaveFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            'source hex string
+
+            For k As Integer = 0 To hex_txt.Length / 2 - 1
+                Mid(hex_bmp, hex_bmp.Length - (k * k * 6 + 3), 2) = hex_txt.Substring(k * 2, 2)
+            Next
+
+            hex_final = hex_bmp
+            '
+            Dim length As Integer = hex_final.Length
+            Dim upperBound As Integer = length \ 2
+            If length Mod 2 = 0 Then
+                upperBound -= 1
+            Else
+                hex_final = "0" & hex_final
+            End If
+            Dim bytes(upperBound) As Byte
+            For i As Integer = 0 To upperBound
+                bytes(i) = Convert.ToByte(hex_final.Substring(i * 2, 2), 16)
+            Next
+
+            Dim fs As System.IO.FileStream
+            fs = New System.IO.FileStream(SaveFileDialog1.FileName, System.IO.FileMode.Create)
+            fs.Write(bytes, 0, bytes.Length)
+            fs.Close()
+
+            Button2.Enabled = False
+            Button3.Enabled = False
+        End If
+    End Sub
+
+
+    Dim hex_bmp_des As String
+    Dim hex_txt_des As String
+    Dim hex_final_des As String
+    Function HexToString(ByVal hex As String) As String
+        Dim text As New System.Text.StringBuilder(hex.Length \ 2)
+        For i As Integer = 0 To hex.Length - 2 Step 2
+            text.Append(Chr(Convert.ToByte(hex.Substring(i, 2), 16)))
+        Next
+        Return text.ToString
+    End Function
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        If OpenFileDialog3.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            hex_bmp_des = String.Join("", IO.File.ReadAllBytes(OpenFileDialog3.FileName).Select(Function(b) b.ToString("X2")).ToArray())
+            Button5.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        If SaveFileDialog2.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            'source hex string
+            '54 info inicial
+            For k As Integer = 0 To (((hex_bmp_des.Length / 2) - 54) / 6) - 1
+                'hex_txt_des = hex_txt_des & HexDeEncrip(hex_bmp_des.Substring(hex_bmp_des.Length - (k * 6 + 3), 2))
+                Dim cabezal As Integer = hex_bmp_des.Length - (k * k * 6 + 4)
+                If hex_bmp_des.Substring(cabezal, 2) = "01" Then
+                    Exit For
+                Else
+                    hex_txt_des = hex_txt_des & hex_bmp_des.Substring(cabezal, 2)
+                End If
+            Next
+
+            hex_final_des = hex_txt_des
+            '
+            Dim length As Integer = hex_final_des.Length
+            Dim upperBound As Integer = length \ 2
+            If length Mod 2 = 0 Then
+                upperBound -= 1
+            Else
+                hex_final_des = "0" & hex_final_des
+            End If
+            Dim bytes(upperBound) As Byte
+            For i As Integer = 0 To upperBound
+                bytes(i) = Convert.ToByte(hex_final_des.Substring(i * 2, 2), 16)
+            Next
+
+            Dim fs As System.IO.FileStream
+            fs = New System.IO.FileStream(SaveFileDialog2.FileName, System.IO.FileMode.Create)
+            fs.Write(bytes, 0, bytes.Length)
+            fs.Close()
+
+            Button5.Enabled = False
+        End If
+    End Sub
+End Class
